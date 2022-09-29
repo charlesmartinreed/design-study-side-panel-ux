e8f1478261f978d5f06d392ce99d1099437217c7
Charles Martin Reed <charlesmartinreed@icloud.com>
2022-09-28 20:00:38 -0500

    Tweak nav animation.

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
 // ELEMENTS
-const navPanel = document.querySelector(".nav-panel");
+const navPanel = document.getElementById("nav-panel");
 const navItems = document.querySelectorAll(".nav-item");
 const albumCards = document.querySelectorAll(".panel-item");
 const albumModal = document.querySelector(".modal-album");
+
+let animDelay = 0.2;
 
 // LISTENERS
 albumCards.forEach((albumCard) => {
@@ -14,14 +16,33 @@ albumCards.forEach((albumCard) => {
 });
 
 navItems.forEach((navItem) => {
-  navItem.addEventListener("mouseenter", (e) => {
-    navItem.classList.add("unfold");
-  });
+  // console.log(navItems.indexOf(navItem));
+  // let animDelay = 0.2;
+  // animDelay += 0.2;
+  // navItem.addEventListener("mouseenter", (e) => {
+  //   triggerNavAnimation();
+  // });
+  // navItem.addEventListener("mouseleave", (e) => {
+  //   removeNavAnimation();
+  // });
+});
 
-  navItem.addEventListener("mouseleave", (e) => {
+navPanel.addEventListener("mouseenter", triggerNavAnimation);
+navPanel.addEventListener("mouseleave", removeNavAnimation);
+
+function triggerNavAnimation(delay) {
+  navItems.forEach((navItem) => {
+    navItem.style.animationDelay = `${delay}s`;
+
+    if (!navItem.classList.contains("unfold")) navItem.classList.add("unfold");
+  });
+}
+
+function removeNavAnimation() {
+  navItems.forEach((navItem) => {
     navItem.classList.remove("unfold");
   });
-});
+}
 
 // MODALS
 function displayAlbumModal(albumID) {
