24347848220a1450eb5dd547b403b34e4ccff68f
Charles Martin Reed <charlesmartinreed@icloud.com>
2022-09-28 19:21:55 -0500

    Animate navbar (using padding-right, bleh)

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 // ELEMENTS
+const navPanel = document.querySelector(".nav-panel");
+const navItems = document.querySelectorAll(".nav-item");
 const albumCards = document.querySelectorAll(".panel-item");
 const albumModal = document.querySelector(".modal-album");
 
@@ -11,7 +13,16 @@ albumCards.forEach((albumCard) => {
   });
 });
 
-console.log(albumModal);
+navItems.forEach((navItem) => {
+  navItem.addEventListener("mouseenter", (e) => {
+    navItem.classList.add("unfold");
+  });
+
+  navItem.addEventListener("mouseleave", (e) => {
+    navItem.classList.remove("unfold");
+  });
+});
+
 // MODALS
 function displayAlbumModal(albumID) {
   // the artist, album details would be pulled in from an API here...
