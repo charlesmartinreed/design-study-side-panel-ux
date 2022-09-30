6ec65bc0dc8c2fca57c030e3c15d353d7f1c5ff1
Charles Martin Reed <charlesmartinreed@icloud.com>
2022-09-29 23:59:16 -0500

    fix modal styling.

--- js/script.js
+++ js/script.js
@@ -5,13 +5,14 @@ const albumCards = document.querySelectorAll(".panel-item");
 const albumModal = document.querySelector(".modal-album");
 
 let animDelay = 0.2;
+let modalIsActive = false;
 
 // LISTENERS
 albumCards.forEach((albumCard) => {
   albumCard.addEventListener("click", (e) => {
     console.log(e.target.parentElement);
     let id = e.target.parentElement.getAttribute("data-album-id");
-    console.log(id);
+    populateModal(id);
   });
 });
 
@@ -44,9 +45,53 @@ function removeNavAnimation() {
   });
 }
 
+window.addEventListener("click", (e) => {
+  if (e.target.classList.contains(".modal-album")) {
+    console.log("modal inner clicked");
+    for (let child of albumModal.children) {
+      if (e.target.matches(child)) {
+        console.log("child clicked");
+        return;
+      }
+    }
+  }
+});
+
 // MODALS
-function displayAlbumModal(albumID) {
+function toggleModal() {
+  modalIsActive = !modalIsActive;
+
+  albumModal.classList.toggle("active", modalIsActive);
+  handleModalInForeground();
+}
+
+function populateModal(albumID) {
+  toggleModal();
+  console.log("populating modal", albumID);
   // the artist, album details would be pulled in from an API here...
-  console.log(albumModal.classList);
-  albumModal.classList.add("active");
+
+  // console.log("modal state is now", modalIsActive);
+}
+
+function handleModalInForeground() {
+  // document.querySelector("body").style.pointerEvents = "none";
+  let pageBody = document.querySelector(".page-container");
+
+  for (let child of pageBody.children) {
+    // console.log(child);
+    if (modalIsActive === true) {
+      child.style.pointerEvents = "none";
+      child.style.filter = "blur(3px)";
+    }
+
+    if (modalIsActive === false) {
+      child.style.pointerEvents = "all";
+      child.style.filter = "none";
+    }
+    // panel-item, nav-panel, 'content-panels
+
+    // console.log(child);
+  }
+
+  // document.querySelector("body").style.filter = "blur(3px)";
 }
